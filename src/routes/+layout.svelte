e71304db3cd9252ca7c17b40720d30563802068d
Shashank Verma <shashank.verma2002@gmail.com>
2025-06-25 22:02:14 +0530

    Init svelte with shadcn
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import "../app.css";
+
+    let { children } = $props();
+</script>
+
+<div class="app">
+    <main>
+        {@render children()}
+    </main>
+
+    <footer>
+        <p>
+            visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to
+            learn about SvelteKit
+        </p>
+    </footer>
+</div>
